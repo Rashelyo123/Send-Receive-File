4a4d9ee1fdcb3ea49aa500c5b1df7c86a05045af
Rashelyo Syawalian <168207952+Rashelyo123@users.noreply.github.com>
2025-09-18 10:22:57 +0700

    Add files via upload

---
+++ clientFile/clientFile/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace clientFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFilename = new System.Windows.Forms.TextBox();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // tbFilename
+            // 
+            this.tbFilename.Location = new System.Drawing.Point(177, 116);
+            this.tbFilename.Name = "tbFilename";
+            this.tbFilename.Size = new System.Drawing.Size(424, 26);
+            this.tbFilename.TabIndex = 1;
+            this.tbFilename.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbServer
+            // 
+            this.tbServer.Location = new System.Drawing.Point(177, 181);
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(424, 26);
+            this.tbServer.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(626, 113);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(90, 32);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(338, 245);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(117, 48);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbServer);
+            this.Controls.Add(this.tbFilename);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbFilename;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ clientFile/clientFile/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace clientFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            tbFilename.Text = openFileDialog.FileName;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = tbFilename.Text;
+                string fileName = Path.GetFileName(filePath); 
+                byte[] fileBuffer = File.ReadAllBytes(filePath);
+
+                TcpClient clientSocket = new TcpClient(tbServer.Text, 8080);
+                NetworkStream networkStream = clientSocket.GetStream();
+                BinaryWriter writer = new BinaryWriter(networkStream);
+
+                
+                writer.Write(fileName);
+               
+                writer.Write(fileBuffer.Length);
+               
+                writer.Write(fileBuffer);
+
+                writer.Close();
+                clientSocket.Close();
+
+                MessageBox.Show(" File berhasil dikirim!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" Error: " + ex.Message);
+            }
+        }
+    }
+}
