f9537ece403db3946b4254168231a6035946707e
Rashelyo Syawalian <168207952+Rashelyo123@users.noreply.github.com>
2025-09-18 10:23:41 +0700

    Add files via upload

---
+++ serverFile/serverFile/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace serverFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbConnection = new System.Windows.Forms.ListBox();
+            this.LBStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbConnection
+            // 
+            this.lbConnection.FormattingEnabled = true;
+            this.lbConnection.ItemHeight = 20;
+            this.lbConnection.Location = new System.Drawing.Point(75, 82);
+            this.lbConnection.Name = "lbConnection";
+            this.lbConnection.Size = new System.Drawing.Size(602, 124);
+            this.lbConnection.TabIndex = 0;
+            // 
+            // LBStatus
+            // 
+            this.LBStatus.AutoSize = true;
+            this.LBStatus.Location = new System.Drawing.Point(71, 27);
+            this.LBStatus.Name = "LBStatus";
+            this.LBStatus.Size = new System.Drawing.Size(56, 20);
+            this.LBStatus.TabIndex = 1;
+            this.LBStatus.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(725, 301);
+            this.Controls.Add(this.LBStatus);
+            this.Controls.Add(this.lbConnection);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ListBox lbConnections;
+        private System.Windows.Forms.ListBox lbConnection;
+        private System.Windows.Forms.Label LBStatus;
+    }
+}
+
---
+++ serverFile/serverFile/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serverFile
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList alSockets;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+            LBStatus.Text = "My IP address is " + IPHost.AddressList[0].ToString();
+
+            alSockets = new ArrayList();
+
+           
+            Thread thdListener = new Thread(new ThreadStart(listenerThread));
+            thdListener.Start();
+        }
+
+        public void listenerThread()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 8080);
+            tcpListener.Start();
+
+            while (true)
+            {
+                Socket handlerSocket = tcpListener.AcceptSocket();
+                if (handlerSocket.Connected)
+                {
+                    lbConnection.Invoke((MethodInvoker)delegate {
+                        lbConnection.Items.Add(handlerSocket.RemoteEndPoint.ToString() + " connected.");
+                    });
+
+                    lock (this)
+                    {
+                        alSockets.Add(handlerSocket);
+                    }
+
+                    Thread thdHandler = new Thread(new ThreadStart(handlerThread));
+                    thdHandler.Start();
+                }
+            }
+        }
+
+        public void handlerThread()
+        {
+            Socket handlerSocket = (Socket)alSockets[alSockets.Count - 1];
+            NetworkStream networkStream = new NetworkStream(handlerSocket);
+            BinaryReader reader = new BinaryReader(networkStream);
+
+           
+            string fileName = reader.ReadString();
+           
+            int fileSize = reader.ReadInt32();
+            
+            byte[] fileBuffer = reader.ReadBytes(fileSize);
+
+          
+            string savePath = Path.Combine("C:\\Users\\Public\\", fileName);
+            File.WriteAllBytes(savePath, fileBuffer);
+
+            lbConnection.Invoke((MethodInvoker)delegate {
+                lbConnection.Items.Add(" File " + fileName + " berhasil diterima dan disimpan di " + savePath);
+            });
+
+            handlerSocket = null;
+        }
+
+    }
+}
